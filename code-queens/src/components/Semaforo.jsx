2e6b5a4c92956ac842bbe0a26cbd7a91472f7c71
Jose Germán Martínez <jgermanmm@gmail.com>
2024-06-03 08:39:45 -0400

    feat: Agregar imagen para el semáforo rojo

--- code-queens/src/components/Semaforo.jsx
+++ code-queens/src/components/Semaforo.jsx
@@ -1,22 +1,58 @@
+import { useState, useEffect } from 'react'
 
 import './Semaforo.css'
 
 
 
 const Semaforo = () => {
+
+ 
+    const [color, setColor] = useState(null);
+   
+    const handleClick = (newColor) => {
+      setColor(color === newColor ? '' : newColor);
+    }
+
+
+
+    // manejar efectos secundarios en el componente después que ha sido renderizado
+    useEffect(()=>{
+    //   setTimeout(() => {
+    //   if(color !== null) {
+    //     setColor(null)
+    //   }
+    // },2000)
+
+    if(color !== null) {
+    alert('El color del semáforo es: ' + color)
+  }
+
+
+    }, [color])
+
+
+
+
+
+ 
+   
   return (
     <>
       <div className="traffic-signal">
+      <div
+        className={`light red ${color === 'red' ? 'glow' : ''}`}
+        onClick={() => handleClick('red')}
+      />
         <div
-          className={"light red "}
+          className={`light yellow ${color === 'yellow' ? 'glow' : ''}`}
+          onClick={() => handleClick('yellow')}
         ></div>
         <div
-          className={"light yellow "}
-        ></div>
-        <div
-          className={"light green "}
+          className={`light green ${color === 'green' ? 'glow' : ''}`}
+          onClick={() => handleClick('green')}
         ></div>
       </div>
+      <button onClick={() => setColor(null)}>Reset</button>
     </>
   )
 }
